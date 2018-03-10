bc004652da3aa0e568bba66b0d3d49b26f925ddd
axano <okmanolos1@hotmail.com>
2018-03-10 17:42:43 -0500

    Create banner.py

---
+++ interface/banner.py
@@ -0,0 +1,13 @@
+#dont forget to escape '\'
+#site used for ascii art generation http://patorjk.com/software/taag/
+def banner():
+    print(""" 
+    _   _                               
+    | | | |                              
+    | |_| | ___ _ __ _ __ ___   ___  ___ 
+    |  _  |/ _ \ '__| '_ ` _ \ / _ \/ __|
+    | | | |  __/ |  | | | | | |  __/\__ \\
+    \_| |_/\___|_|  |_| |_| |_|\___||___/
+     
+    SNMP scanning tool                      """)
+    return
