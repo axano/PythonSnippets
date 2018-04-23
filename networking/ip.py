aca0a99f07856da0189c36879d282c6725120d8f
Emmanouil Perselis <okmanolos1@hotmail.com>
2018-04-23 19:24:54 -0400

    Create ip.py

---
+++ networking/ip.py
@@ -0,0 +1,5 @@
+import socket
+
+#works on LANs without an internet connection
+def getLocalIp():
+    return((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
