9fdd08fda2099dc1ddd2ee5c239d7c3d59b02701
Emmanouil Perselis <okmanolos1@hotmail.com>
2018-04-23 20:09:24 -0400

    Update ip.py

--- networking/ip.py
+++ networking/ip.py
@@ -1,4 +1,7 @@
 import socket
+import fcntl
+import struct
+
 
 #Does NOT need routable net access or any connection at all.
 #Works even if all interfaces are unplugged from the network.
@@ -27,3 +30,19 @@ def getLocalIp():
 #Tested on UNIX-like OS's
 def getNetmaskByNic(ifname):
     return socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack(bytes('256s','utf-8'), bytes(ifname,'utf-8')))[20:24])
+
+#Searches the iface that is connected to the gateway
+#by looking it up in /proc/net/route
+#Tested in python 3.6
+#Tested on UNIX-like OS's
+def getDefaultIfaceNameLinux():
+    route = "/proc/net/route"
+    with open(route) as f:
+        for line in f.readlines():
+            try:
+                iface, dest, _, flags, _, _, _, _, _, _, _, =  line.strip().split()
+                if dest != '00000000' or not int(flags, 16) & 2:
+                    continue
+                return iface
+            except:
+                continu
